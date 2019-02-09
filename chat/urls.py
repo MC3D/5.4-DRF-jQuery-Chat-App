cf9a9bd8b641923c5f99f77e51dcf14c55e6d967
MC3D <m.chapman.home@gmail.com>
2019-02-09 14:42:46 -0500

    adds chat IndexView

--- chat/urls.py
+++ chat/urls.py
@@ -7,5 +7,5 @@ from . import views
 app_name = "chat"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+#
+#
+# def index():
+#     return HttpResponse('Hello Chat App!')
+
+from django.views.generic import TemplateView
 
 
-def index(requxest):
-    return HttpResponse('Hello Chat App!')
+class IndexView(TemplateView):
+    template_name = 'chat/index.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = {
+            'greeting': 'Hello',
+            'name': 'World'
+        }
+
+        return context
