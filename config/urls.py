35aecfb8cec7e94a8274871f932f39ad9f02abd6
MC3D <m.chapman.home@gmail.com>
2019-02-09 14:16:44 -0500

    modifies url patterns

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-from chat import views
-
-
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     # path('api', include('api.urls')),
-    # path('chat', include('chat.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('chat.urls')),
 ]
