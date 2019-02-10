dd25b68d1ad137de4145e97dc407cd58de4a0d6d
MC3D <m.chapman.home@gmail.com>
2019-02-10 10:44:39 -0500

    adds message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+# https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#referencing-the-user-model
+
+
+class Message(models.Model):
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    text = models.CharField(max_length=264,)
+    created = models.DateTimeField(auto_now_add=True,)
+
+    # string representation of your model so you can print out an instance if you want
+    def __str__(self):
+        return self.text
--- chat/views.py
+++ chat/views.py
@@ -1,27 +1,38 @@
 from django.views.generic import TemplateView
 
-
-class IndexView(TemplateView):
-    template_name = 'chat/index.html'
-
-    def get_context_data(self, **kwargs):
-
-        context = {
-            'greeting': 'Hello',
-            'name': 'World'
-        }
-
-        return context
+from chat.models import Message
 
 
-class ChatView(TemplateView):
+# class IndexView(TemplateView):
+#     template_name = 'chat/index.html'
+#
+#     def get_context_data(self, **kwargs):
+#
+#         context = {
+#             'greeting': 'Hello',
+#             'name': 'World'
+#         }
+#
+#         return context
+
+
+# class ChatView(TemplateView):
+#     template_name = 'chat/chat.html'
+#
+#     def get_context_data(self, **kwargs):
+#
+#         context = {
+#             'greeting': 'Hello',
+#             'name': 'Django'
+#         }
+#
+#         return context
+
+
+class ChatIndex(TemplateView):
     template_name = 'chat/chat.html'
 
     def get_context_data(self, **kwargs):
-
-        context = {
-            'greeting': 'Hello',
-            'name': 'Django'
-        }
-
+        messages = Message.objects.order_by('created')
+        context = {'messages': messages}
         return context
